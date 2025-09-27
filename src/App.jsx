156b34c25a1846856d5576fe6f6768ae6abee30e
jdtrystocode <singhvijayaditya13@gmail.com>
2025-09-27 15:39:43 +0530

    Added Login and Register page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
 import ProfilePage from "./routes/profilePage/profilePage";
+import Register from "./routes/register/register";
+import Login from "./routes/login/login";
 
 function App() {
   
@@ -31,6 +33,14 @@ function App() {
       {
         path:"/profile",
         element:<ProfilePage/>
+      },
+      {
+        path:"/register",
+        element:<Register/>
+      },
+      {
+        path:"/login",
+        element:<Login/>
       }
     ]
   },
